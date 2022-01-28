c2724b727ff038e59b0dd501404e696d144f348e
Eric Korth <ebkorth7@gmail.com>
2022-01-28 09:29:44 -0500

    Editing build files

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 export default function Navigation(props) {
     return (
@@ -8,27 +8,23 @@ export default function Navigation(props) {
                 <NavbarBrand href='#'>
                     <img className='brandLogo' src='/assets/images/ebklogo.png' />
                 </NavbarBrand>
-                {/* <NavbarToggler>
-                    <Collapse> */}
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className='navItem' href="#about">
-                                About
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='navItem' href="#portfolio">
-                                Portfolio
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className='navItem' href="#contact">
-                                Contact
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    {/* </Collapse>
-                </NavbarToggler> */}
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink className='navItem' href="#about">
+                            About
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='navItem' href="#portfolio">
+                            Portfolio
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='navItem' href="#contact">
+                            Contact
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
             </Navbar>
         </section>
     );
